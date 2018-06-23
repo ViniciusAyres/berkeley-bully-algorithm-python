ba23481875e7ab377abddf4b15d96718b4817df5
Vinicius <viniciusayres.sg@gmail.com>
2018-06-23 16:48:15 -0300

    [add] Criação de classe para ser o relógio do processo

---
+++ utils/Timer.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+import time
+import threading
+
+class Timer:
+	def __init__(self, time, increment=1, secondsToUpdate=1):
+		self.time = time
+		self.increment = increment
+		self.secondsToUpdate = secondsToUpdate
+
+		time_thread = threading.Thread(target=self.__start)
+		time_thread.start()
+
+	def getTime(self):
+		return self.time
+
+	def setTime(self, time):
+		self.time = time
+
+	def updateTime(self):
+		self.time = self.time + timedelta(seconds=self.increment)
+
+	def __start(self):
+		while(True):
+			self.updateTime()
+			time.sleep(self.secondsToUpdate)
